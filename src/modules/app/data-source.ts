780cd22daec3e0b2002f41c40dc80f591f3fc06b
blackparadise0407 <blackparadise0407@gmail.com>
2022-04-15 18:08:27 +0700

    Change env var

--- src/modules/app/data-source.ts
+++ src/modules/app/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '040799',
-  database: process.env.DB_NAME || 'test',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || '040799',
+  database: process.env.POSTGRES_DB || 'test',
   synchronize: true,
   entities: [path.join(__dirname, '..', '**/*.entity.{js,ts}')],
   subscribers: [],
